c0f1ead942be4af97b92c74764c14cae18c7685d
mpruvot <132161864+mpruvot@users.noreply.github.com>
2023-09-16 12:59:23 +0200

    delete method working + test

--- test_delete.py
+++ test_delete.py
@@ -28,6 +28,31 @@ class UserManager:
                 print(f'{user_type}: {name} succefully deleted !')
             else:
                 raise UserDoesNotExist(f"{user_type} {name} does not exist !")
-        
-
+            
     
+
+manager = UserManager()
+
+############# TESTS ###############
+
+print('TEST CREATE ADMIN')
+try:
+    manager.new(UserType.ADMIN, "Marius")
+except UserAlreadyExist as e:
+    print(e)
+print(f'USERLIST >>>>>>>>>>>>>>>>> {manager.user_list}')
+
+print('TEST DELETE ADMIN')
+try:
+    manager.delete(UserType.ADMIN, "Marius")
+except UserDoesNotExist as e:
+    print(e)
+print(f'USERLIST >>>>>>>>>>>>>>>>> {manager.user_list}')
+    
+print('TEST DELETE ADMIN WHO DOES NOT EXIST')
+try:
+    manager.delete(UserType.ADMIN, "Marius")
+except UserDoesNotExist as e:
+    print(e)
+print(f'USERLIST >>>>>>>>>>>>>>>>> {manager.user_list}')
+
